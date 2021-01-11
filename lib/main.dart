3b569f96b78ec6c1228b268bf141d7edd7702205
curiousyuvi <ysgaur9919@gmail.com>
2021-01-11 19:55:15 +0530

    made a hello World Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//main() is the starting point of all are flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
